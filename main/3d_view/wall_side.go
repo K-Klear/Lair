140605e0b17e9217b50b3b58f9172c876ad589e8
Kklear <kingklear@gmail.com>
2019-10-12 15:19:58 +0200

    So I did something with a shader and it might be usable. Actually, I think it's almost "cheating", if I can get it working. We'll see.

--- main/3d_view/wall_side.go
+++ main/3d_view/wall_side.go
@@ -2,13 +2,8 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/3d_view/dungeon.atlas\"\n"
-<<<<<<< HEAD
-  "default_animation: \"wall_front\"\n"
+  "default_animation: \"wall_test\"\n"
   "material: \"/shaders/skew.material\"\n"
-=======
-  "default_animation: \"wall_side\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
->>>>>>> 417c7c44d5796350e09be2d2bc92f0c894aaa68a
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
