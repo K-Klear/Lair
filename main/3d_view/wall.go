9a78c2d4b1cdf51e1105c3c23d280368db68f7e0
Kklear <kingklear@gmail.com>
2019-10-23 17:43:38 +0200

    Done with the shaders. Wall textures are borked, but it's good enough for me.
    FoV culling is next.

--- main/3d_view/wall.go
+++ main/3d_view/wall.go
@@ -14,35 +14,15 @@ components {
   }
 }
 embedded_components {
-  id: "front"
+  id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/3d_view/dungeon.atlas\"\n"
-  "default_animation: \"wall_front\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: -1.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "left"
-  type: "sprite"
-  data: "tile_set: \"/main/3d_view/dungeon.atlas\"\n"
-  "default_animation: \"wall_test\"\n"
+  "default_animation: \"wall\"\n"
   "material: \"/graphics/shaders/skew.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
-    x: 0.0
+    x: 121.5
     y: 0.0
     z: 0.0
   }
